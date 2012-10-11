de2b4727cd4d22a5bdf753e25c17b352276c86c5
Kerry Perez <k4torce@gmail.com>
2012-10-11 11:42:40 -0500

    actualizando archivos borrados

--- src/Configuracion/ConfiguracionIp.java
+++
@@ -1,6 +0,0 @@
-package Configuracion;
-
-public class ConfiguracionIp {
-	public static String ip="http://trustripes.com/dev";//respetar los //.
-
-}
--- src/com/creatiwebs/trustripes/MyApp.java
+++
@@ -1,7 +0,0 @@
-package com.creatiwebs.trustripes;
-
-import android.app.Application;
-
-public class MyApp extends Application {
-
-}
