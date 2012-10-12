f1fd9bb3b917a38fa43978ba8340a3535af0476f
teto100 <antoniomendoz3@gmail.com>
2012-10-12 10:59:27 -0500

    Cambios 11-10

--- gen/com/creatiwebs/trustripes/R.java
+++ gen/com/creatiwebs/trustripes/R.java
@@ -13,16 +13,22 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int splashscreen_bg=0x7f020002;
-        public static final int splashscreen_footer=0x7f020003;
-        public static final int splashscreen_logo=0x7f020004;
-        public static final int splashscreen_logo2=0x7f020005;
+        public static final int login_logo_small=0x7f020002;
+        public static final int splashscreen_bg=0x7f020003;
+        public static final int splashscreen_footer=0x7f020004;
+        public static final int splashscreen_logo=0x7f020005;
     }
     public static final class id {
-        public static final int footer=0x7f070002;
-        public static final int logo=0x7f070000;
-        public static final int menu_settings=0x7f070003;
-        public static final int splash_screen_progressBar=0x7f070001;
+        public static final int footer=0x7f070006;
+        public static final int inner_container=0x7f070000;
+        public static final int login_facebook_button=0x7f070004;
+        public static final int login_password=0x7f070003;
+        public static final int login_twitter_button=0x7f070005;
+        public static final int login_username=0x7f070002;
+        public static final int logo=0x7f070007;
+        public static final int mainActivity_logo=0x7f070001;
+        public static final int menu_settings=0x7f070009;
+        public static final int splash_screen_progressBar=0x7f070008;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -37,10 +43,11 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int mainActivity_imgView_logo_contentDescription=0x7f040007;
         public static final int menu_settings=0x7f040002;
         public static final int splashScreen_imgView_footer_contentDescription=0x7f040005;
         public static final int splashScreen_imgView_logo_contentDescription=0x7f040006;
-        public static final int title_activity_login=0x7f040007;
+        public static final int title_activity_login=0x7f040008;
         public static final int title_activity_main=0x7f040003;
         public static final int title_activity_splash=0x7f040004;
     }
