68a1a3ccdf1695f47796299722aba07c725555bd
teto100 <antoniomendoz3@gmail.com>
2012-10-15 13:06:22 -0500

    Interfaz de la Actividad Login terminado

--- gen/com/creatiwebs/trustripes/R.java
+++ gen/com/creatiwebs/trustripes/R.java
@@ -11,12 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int login_logo_small=0x7f020002;
-        public static final int splashscreen_bg=0x7f020003;
-        public static final int splashscreen_footer=0x7f020004;
-        public static final int splashscreen_logo=0x7f020005;
+        public static final int bg_with_footer=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int login_logo_small=0x7f020003;
+        public static final int splashscreen_bg=0x7f020004;
+        public static final int splashscreen_footer=0x7f020005;
+        public static final int splashscreen_logo=0x7f020006;
     }
     public static final class id {
         public static final int footer=0x7f070006;
