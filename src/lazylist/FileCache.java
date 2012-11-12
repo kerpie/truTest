b7be202295f75300bd3acd095655f49938639504
teto100 <antoniomendoz3@gmail.comexit>
2012-11-12 12:50:28 -0500

    Image Cache Modified

--- src/lazylist/FileCache.java
+++ src/lazylist/FileCache.java
@@ -6,15 +6,19 @@ import android.content.Context;
 public class FileCache {
     
     private File cacheDir;
+	private File dire;
     
     public FileCache(Context context){
         //Find the dir to save cached images
-        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
-            cacheDir=new File(android.os.Environment.getExternalStorageDirectory(),"truStripes");
+    	
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)){
+            cacheDir=new File(android.os.Environment.getExternalStorageDirectory(),"TruStripes");
+            dire = new File(cacheDir, "Cache");    	
+        }
         else
-            cacheDir=context.getCacheDir();
-        if(!cacheDir.exists())
-            cacheDir.mkdirs();
+            dire=context.getCacheDir();
+        if(!dire.exists())
+            dire.mkdirs();
     }
     
     public File getFile(String url){
@@ -22,13 +26,13 @@ public class FileCache {
         String filename=String.valueOf(url.hashCode());
         //Another possible solution (thanks to grantland)
         //String filename = URLEncoder.encode(url);
-        File f = new File(cacheDir, filename);
+        File f = new File(dire, filename);
         return f;
         
     }
     
     public void clear(){
-        File[] files=cacheDir.listFiles();
+        File[] files=dire.listFiles();
         if(files==null)
             return;
         for(File f:files)
